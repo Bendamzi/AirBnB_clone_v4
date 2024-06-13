bf370a85ecba5fd3df63696299b5e5d70b1c8d85
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-06-13 13:35:20 +0100

    My commit

---
+++ web_dynamic/static/images/scripts/styles/1-hbnb.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/* Houses jquery api that scripts the frontend of a flask app, defined in 1-hbnb.py, inside web_dynamic directory */
+$(document).ready(function () {
+    const filters = [];
+    $.each($('.popover ul li input'), function (ind, val) {
+      $(val).click(function () {
+        const h4Element = $('DIV.amenities H4');
+        let texts = '';
+        if ($(val).is(':checked')) {
+          filters.push($(this).attr('data-name'));
+        } else {
+          filters.splice(filters.indexOf($(this).attr('data-name')), 1);
+        }
+        $.each(filters, function (index, value) {
+          if (texts.length < 2) {
+            texts = texts.concat(value);
+          } else {
+            texts = texts.concat(', ');
+            texts = texts.concat(value);
+          }
+        });
+        if (texts.length > 30) {
+          texts = texts.substring(0, 30) + '...';
+        }
+        if (filters.length < 1) {
+          h4Element.html('&nbsp;');
+        } else {
+          h4Element.text(texts);
+        }
+      });
+    });
+  });
+  
